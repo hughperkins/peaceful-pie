b23419e7dacfa56a8e107c388cb47349af672e91
Hugh Perkins <hughperkins@gmail.com>
2022-12-30 06:38:54 -0500

    tweak unity path

--- ci/build-ci.sh
+++ ci/build-ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i -e 's/\/Applications\/Unity\/Editor\/2021.3.16f1\/Unity.app\/Contents/\/opt\/Unity\/Editor\/Data/g' PeacefulPie/PeacefulPie.csproj
+sed -i -e 's/\/Applications\/Unity\/Editor\/2021.3.16f1\/Unity.app\/Contents/\/opt\/Unity\/Editor\/Data\/Managed\/UnityEngine/g' PeacefulPie/PeacefulPie.csproj
 dotnet build PeacefulPie &&  \
     cp PeacefulPie/bin/Debug/netstandard2.1/PeacefulPie.dll .
