e15f1f89e313225fd6f5b28667f4156c9cd25807
Hugh Perkins <hughperkins@gmail.com>
2022-12-30 06:40:00 -0500

    dump proj

--- ci/build-ci.sh
+++ ci/build-ci.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -x
+
 sed -i -e 's/\/Applications\/Unity\/Editor\/2021.3.16f1\/Unity.app\/Contents/\/opt\/Unity\/Editor\/Data\/Managed\/UnityEngine/g' PeacefulPie/PeacefulPie.csproj
+cat PeacefulPie/PeacefulPie.csproj
 dotnet build PeacefulPie &&  \
     cp PeacefulPie/bin/Debug/netstandard2.1/PeacefulPie.dll .
