8caa34a99b1e8af3b50589dc053c48bffc82d043
Hugh Perkins <hughperkins@gmail.com>
2022-12-30 15:15:48 -0500

    add a lot of configuration options to simulation

--- PeacefulPie/Simulation.cs
+++ PeacefulPie/Simulation.cs
@@ -1,65 +1,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface INeedFixedUpdate{
-    void MyFixedUpdate(float deltaTime);
-    bool IsAlive();
+public interface INeedFixedUpdate {
+	void MyFixedUpdate(float deltaTime);
+	bool IsAlive();
 }
 
-public interface INeedUpdate{
-    void MyUpdate(float deltaTime);
-    bool IsAlive();
+public interface INeedUpdate {
+	void MyUpdate(float deltaTime);
+	bool IsAlive();
 }
 
 public class Simulation : MonoBehaviour {
-    // handles running physics etc
-    // will step automatically when no AI runnnig, otherwise on command of
-    // rl rpc incoming step event
+	// handles running physics etc
+	// will step automatically when no AI runnnig, otherwise on command of
+	// rl rpc incoming step event
 
-    [Header("Size of each simulation step, in seconds of game time.")]
-    public float SimulationStepSize = 0.02f;
-    [Tooltip("Automatically a simulation step each FixedStep?")]
-    public bool AutoRunSimulations = true;
+	[Header("Size of each simulation step, in seconds of game time.")]
+	public float SimulationStepSize = 0.02f;
+	[Header("Size of each simulation step, in real time. Make this smaller than Simulation Step Size to accelerate the game.")]
+	public float RealStepSize = 0.002f;
+	[Tooltip("Automatically run a simulation step each FixedStep?")]
+	public bool AutoRunSimulations = false;
 
-    List<INeedFixedUpdate> registeredNeedFixedUpdates = new List<INeedFixedUpdate>();
-    List<INeedUpdate> registeredNeedUpdates = new List<INeedUpdate>();
+	List<INeedFixedUpdate> registeredNeedFixedUpdates = new List<INeedFixedUpdate>();
+	List<INeedUpdate> registeredNeedUpdates = new List<INeedUpdate>();
 
-    public void RegisterNeedFixedUpdate(INeedFixedUpdate needFixedUpdate) {
-        registeredNeedFixedUpdates.Add(needFixedUpdate);
-    }
+	private void Awake() {
+		Physics.autoSimulation = false;
+		Debug.Log("Turning off physics autosimulation");
+	}
 
-    public void RegisterNeedUpdate(INeedUpdate needUpdate) {
-        registeredNeedUpdates.Add(needUpdate);
-    }
+	public void RegisterNeedFixedUpdate(INeedFixedUpdate needFixedUpdate) {
+		registeredNeedFixedUpdates.Add(needFixedUpdate);
+	}
 
-    public void RunFixedUpdates(float deltaTime) {
-        foreach(INeedFixedUpdate needsFixedUpdate in registeredNeedFixedUpdates) {
-            if(needsFixedUpdate.IsAlive())  {
-                needsFixedUpdate.MyFixedUpdate(deltaTime);
-            }
-        }
-    }
+	public void RegisterNeedUpdate(INeedUpdate needUpdate) {
+		registeredNeedUpdates.Add(needUpdate);
+	}
 
-    public void RunUpdates(float deltaTime) {
-        foreach(INeedUpdate needsUpdate in registeredNeedUpdates) {
-            if(needsUpdate.IsAlive()) {
-                needsUpdate.MyUpdate(deltaTime);
-            }
-        }
-    }
+	public void RunFixedUpdates(float deltaTime) {
+		foreach(INeedFixedUpdate needsFixedUpdate in registeredNeedFixedUpdates) {
+			if(needsFixedUpdate.IsAlive()) {
+				needsFixedUpdate.MyFixedUpdate(deltaTime);
+			}
+		}
+	}
 
-    void FixedUpdate() {
-        if(AutoRunSimulations) {
-            Simulate();
-        }
-    }
+	public void RunUpdates(float deltaTime) {
+		foreach(INeedUpdate needsUpdate in registeredNeedUpdates) {
+			if(needsUpdate.IsAlive()) {
+				needsUpdate.MyUpdate(deltaTime);
+			}
+		}
+	}
 
-    public void Simulate() {
-        float deltaTime = SimulationStepSize;
-        RunFixedUpdates(deltaTime);
-        RunUpdates(deltaTime);
-        if(!Physics.autoSimulation) {
-            Physics.Simulate(deltaTime);
-        }
-    }
+	void FixedUpdate() {
+		if(AutoRunSimulations) {
+			Simulate();
+		}
+	}
+
+	public void Simulate() {
+		float deltaTime = SimulationStepSize;
+		RunFixedUpdates(deltaTime);
+		RunUpdates(deltaTime);
+		if(!Physics.autoSimulation) {
+			Physics.Simulate(deltaTime);
+		}
+	}
 }
