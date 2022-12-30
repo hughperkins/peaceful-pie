2c8baf0524c1d25403f1a41a839b8ead5da35091
Hugh Perkins <hughperkins@gmail.com>
2022-12-30 06:44:18 -0500

    hopefuly get path right this time

--- ci/build-ci.sh
+++ ci/build-ci.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-sed -i -e 's/\/Applications\/Unity\/Editor\/2021.3.16f1\/Unity.app\/Contents/\/opt\/Unity\/Editor\/Data\/Managed\/UnityEngine/g' PeacefulPie/PeacefulPie.csproj
+sed -i -e 's/\/Applications\/Unity\/Hub\/Editor\/2021.3.16f1\/Unity.app\/Contents\/Managed\/UnityEngine/\/opt\/Unity\/Editor\/Data\/Managed\/UnityEngine/g' PeacefulPie/PeacefulPie.csproj
 cat PeacefulPie/PeacefulPie.csproj
 dotnet build PeacefulPie &&  \
     cp PeacefulPie/bin/Debug/netstandard2.1/PeacefulPie.dll .
