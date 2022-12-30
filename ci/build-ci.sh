78dc5f3fe9889a3c90a74f033922264dbe1798b7
Hugh Perkins <hughperkins@gmail.com>
2022-12-30 06:32:51 -0500

    try ci build

---
+++ ci/build-ci.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sed -i -e 's/\/Applications\/Unity\/Editor\/2021.3.16f1\/Unity.app\/Contents/\/opt\/Unity\/Editor\/Data/g' PeacefulPie/PeacefulPie.csproj
+dotnet build PeacefulPie &&  \
+    cp PeacefulPie/bin/Debug/netstandard2.1/PeacefulPie.dll .
