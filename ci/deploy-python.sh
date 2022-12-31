debc9c02f12be9d2b05aa3f921e7dd2eab620cdd
Hugh Perkins <hughperkins@gmail.com>
2022-12-31 09:38:59 -0500

    trya agin

--- ci/deploy-python.sh
+++ ci/deploy-python.sh
@@ -8,13 +8,18 @@ if command -v pyenv 1>/dev/null 2>&1; then
 fi
 eval "$(pyenv virtualenv-init -)"
 
+pyenv virtualenv 3.10.9 myenv
+pyenv activate myenv
+git status
+
+pip install colorama
+pip install boto3
+pip freeze
+
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | POETRY_UNINSTALL=1 python -
 curl -sSL https://install.python-poetry.org | python3 -
 
 poetry self add "poetry-dynamic-versioning[plugin]"
 
-pyenv virtualenv 3.10.9 myenv
-pyenv activate myenv
-git status
 poetry install --verbose --no-interaction
 poetry build -v
