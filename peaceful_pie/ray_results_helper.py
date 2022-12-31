8fe2c43506e38dab55563e8cc771f0be943811ef
Hugh Perkins <hughperkins@gmail.com>
2022-12-31 12:13:37 -0500

    add some docstring to ray_results_helper.py

--- peaceful_pie/ray_results_helper.py
+++ peaceful_pie/ray_results_helper.py
@@ -18,10 +18,20 @@ def ray_results_to_feature_np(
     Takes in the ray results, i.e. hit distances and object types,
     and return a single numpy array, of same shape as each of distances and object types,
     but with an additional 0th dimension of length object_types
-    the distances will be inverted, so that nearer gives a higher number, and further gives
+
+    Conceptually, if a number is zero, that ray didnt hit that tag type. If it is near zero,
+    it hit that tag type, but it was far away. If it is non-zero and near 1, then the ray hit
+    that tag type, and the object was relatively near. So:
+    - 0 => tag not seen
+    - near 0 => tag far away
+    - near 1 => tag nearby
+
+    The number is not upper-bounded: we simply take the inverse of the distance. However, you can
+    always apply your own sigmoid or similar, to the output of this function, to bound it.
+
+    In detail: the distances will be inverted, so that nearer gives a higher number, and further gives
     smaller. this number will be assigned to the output plane indexed by object type.
-    If object type is -1, the number will be ignored, and output will be 0 for all output
-    channels
+    If object type is -1, the output will be set to 0 across all output channels
     """
     distances_np = np.array(ray_results.rayDistances)
     distances_np = 1 / distances_np
