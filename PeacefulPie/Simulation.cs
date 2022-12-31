5d7d0779ac53c4437df16a0a1e703f0b7ea0e543
Hugh Perkins <hughperkins@gmail.com>
2022-12-31 07:36:12 -0500

    deal with dedicated server framerates

--- PeacefulPie/Simulation.cs
+++ PeacefulPie/Simulation.cs
@@ -16,10 +16,17 @@ public class Simulation : MonoBehaviour {
 	// will step automatically when no AI runnnig, otherwise on command of
 	// rl rpc incoming step event
 
-	[Header("Size of each simulation step, in seconds of game time.")]
+	[Tooltip("Size of each simulation step, in seconds of game time.")]
 	public float SimulationStepSize = 0.02f;
-	[Header("Size of each simulation step, in real time. Make this smaller than Simulation Step Size to accelerate the game. Must be more than 0.")]
+	[Tooltip("Size of each simulation step, in real time. Make this smaller than Simulation Step Size to accelerate the game. Must be more than 0. " +
+		"You need to click Apply Real Step Size below, to apply any changes at runtime.")]
 	public float RealStepSize = 0.005f;
+	[Tooltip("Click this to apply the Real Step Size you set above, during runtime. (This is to avoid accidentally setting it to 0)")]
+	public bool ApplyRealStepSize = false;
+	[Tooltip("Size of each simulation step, in real time, on a dedicated server. Typically should be really small, e.g. 0.0001.")]
+	public float DedicatedRealStepSize = 0.0001f;
+	[Tooltip("Number of Updates to run per second, on a dedicated server. Typically should be small, such as 10.")]
+	public int DedicatedTargetFrameRate = 10;
 	[Tooltip(
 		"Automatically run a simulation step each FixedStep? " +
 		"When under Python control, we usually want the Python will control when to run a simulation step, so this should be off.")]
@@ -28,16 +35,35 @@ public class Simulation : MonoBehaviour {
 	List<INeedFixedUpdate> registeredNeedFixedUpdates = new List<INeedFixedUpdate>();
 	List<INeedUpdate> registeredNeedUpdates = new List<INeedUpdate>();
 
+	public bool isDedicated() {
+		return Screen.currentResolution.refreshRate == 0;
+	}
+
 	private void OnValidate() {
-		if(RealStepSize < 0.001f) {
-			RealStepSize = 0.005f;
+		if(ApplyRealStepSize)
+		{
+			if(RealStepSize == 0.0f) {
+				Debug.LogError("RealStepSize should not be 0");
+			} else
+			{
+				Time.fixedDeltaTime = RealStepSize;
+				ApplyRealStepSize = false;
+				Debug.Log($"apply fixed delta time {Time.fixedDeltaTime}");
+			}
 		}
-		Time.fixedDeltaTime = RealStepSize;
 	}
 
 	private void Awake() {
 		Debug.Log("Turning off physics autosimulation");
 		Physics.autoSimulation = false;
+
+		Debug.Log("is dedicated " + isDedicated());
+		if(isDedicated()) {
+			Application.targetFrameRate = DedicatedTargetFrameRate;
+			Debug.Log($"Set application target framerate to {Application.targetFrameRate}");
+			RealStepSize = DedicatedRealStepSize;
+			Debug.Log($"Set RealStepSize to {RealStepSize}");
+		}
 		Debug.Log($"Setting fixed delta time to {RealStepSize}");
 		Time.fixedDeltaTime = RealStepSize;
 	}
