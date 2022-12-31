285418f454053ddcce9d761606b004599f30859f
Hugh Perkins <hughperkins@gmail.com>
2022-12-31 09:37:14 -0500

    try reinstalling poetry

--- ci/deploy-python.sh
+++ ci/deploy-python.sh
@@ -8,6 +8,9 @@ if command -v pyenv 1>/dev/null 2>&1; then
 fi
 eval "$(pyenv virtualenv-init -)"
 
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | POETRY_UNINSTALL=1 python -
+curl -sSL https://install.python-poetry.org | python3 -
+
 poetry self add "poetry-dynamic-versioning[plugin]"
 
 pyenv virtualenv 3.10.9 myenv
