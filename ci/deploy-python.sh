3b94a86580fcb0b9d4d90187f21b5120b976770a
Hugh Perkins <hughperkins@gmail.com>
2022-12-31 10:06:09 -0500

    only deploy on tags

--- ci/deploy-python.sh
+++ ci/deploy-python.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -x
 set -e
 
 if command -v pyenv 1>/dev/null 2>&1; then
@@ -10,26 +9,11 @@ eval "$(pyenv virtualenv-init -)"
 
 pyenv virtualenv 3.10.9 myenv
 pyenv activate myenv
-git status
 
-# env
-
-# pip install colorama
-# pip install boto3
-# pip freeze
-
-# pip install .
-# pip freeze
-# python -c 'import peaceful_pie'
-# python -c 'import peaceful_pie; print(peaceful_pie.__version__)'
+set -x
 
 poetry self update --quiet
-
-# curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | POETRY_UNINSTALL=1 python -
-# curl -sSL https://install.python-poetry.org | python3 -
-
 poetry self add --quiet "poetry-dynamic-versioning[plugin]"
-
 poetry install --quiet --no-interaction
 
 python -c 'import peaceful_pie'
