ce4b94acac697f548c3aa4616804094734c8fa0e
Hugh Perkins <hughperkins@gmail.com>
2022-12-31 08:22:44 -0500

    activate pyenv

--- ci/deploy-python.sh
+++ ci/deploy-python.sh
@@ -3,6 +3,11 @@
 set -x
 set -e
 
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
+eval "$(pyenv virtualenv-init -)"
+
 pyenv virtualenv 3.10.9 myenv
 pyenv activate myenv
 poetry install
